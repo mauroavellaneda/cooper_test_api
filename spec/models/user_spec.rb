cfbe0f3dda0c16ad909710cd8261ff8902345b36
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-16 18:00:22 +0200

    this is a testing branch with the only purpose of coding along with my team mate. Nothing here is going to be merged with the master or any other branch

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe User, type: :model do
   it 'should have valid Factory' do
     expect(create(:user)).to be_valid
   end
+  describe 'Relations' do
+    it { is_expected.to have_many :performance_data }
+  end
   describe 'Database table' do
     it { is_expected.to have_db_column :encrypted_password }
     it { is_expected.to have_db_column :email }
