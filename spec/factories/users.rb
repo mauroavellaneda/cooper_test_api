5501bcbbd054f7a522f01c341bf898479ca292e1
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-17 19:45:02 +0200

    minor changes in order to pass the tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     
-    email { "user@random.com" }
+    email { "user@mail.com" }
     password { "password" }
     password_confirmation { "password" }
     
